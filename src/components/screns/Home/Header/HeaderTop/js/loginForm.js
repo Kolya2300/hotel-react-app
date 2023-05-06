384a48e51f7dc84717d16628d47ba328d939f3f8
Kolya2300 <84928896+Kolya2300@users.noreply.github.com>
2023-05-06 22:14:48 +0300

    Delete src directory

--- src/components/screns/Home/Header/HeaderTop/js/loginForm.js
+++
@@ -1,7 +0,0 @@
-export function showForm(){
-    const loginBtn = document.querySelector('.btnLogin_popup')
-    loginBtn.addEventListener('click', (e)=>{
-        e.preventDefault()
-        alert('Click')
-    })
-}
